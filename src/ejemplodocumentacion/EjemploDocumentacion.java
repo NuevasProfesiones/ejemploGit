ca6228493b474a2963b259ac529ed58e743cdc55
Your Name <you@example.com>
2017-02-13 10:01:30 +0100

    Subida de Proyecto

---
+++ src/ejemplodocumentacion/EjemploDocumentacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplodocumentacion;
+
+/**
+ *
+ * @author Administrador
+ * Esta clase es para ser usada de ejemplo de documentación de JAVA
+ * 
+ */
+public class EjemploDocumentacion {
+
+    /**
+     * Este es el método principal de la clase para ser ejecutado.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/ejemplodocumentacion/paquete2/Clase2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplodocumentacion.paquete2;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Clase2 {
+    
+/**
+ * esta funcion tiene una suma
+ * @param a el numero de veces que el usuario...
+ * @param b por el numero de no se que
+ * @return  No devuelve nada
+ * @throws Exception
+ */
+    public void suma(int a, int b) throws Exception{
+    
+    
+    }
+}
